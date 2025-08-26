08977a564f34d1848e2380b083c723c74d3c4717
danielly7 <160144031+danielly7@users.noreply.github.com>
2025-08-26 13:57:17 -0300

    26/08

--- main.js
+++ main.js
@@ -7,40 +7,39 @@ const botoes = document.querySelectorAll('.parametro-senha__botao');
 botoes[0].onclick = diminuiTamanho;
 botoes[1].onclick = aumentaTamanho;
 
-function diminuiTamanho(){
-    if(tamanhoSenha > 1){
+function diminuiTamanho() {
+    if (tamanhoSenha > 1) {
         tamanhoSenha = tamanhoSenha - 1
     }
-    numeroSenha.textContent=tamanhoSenha;
+    numeroSenha.textContent = tamanhoSenha;
     geraSenha();
 }
 
-function aumentaTamanho(){
-    if(tamanhoSenha < 20){
+function aumentaTamanho() {
+    if (tamanhoSenha < 20) {
         tamanhoSenha = tamanhoSenha + 1
     }
-    numeroSenha.textContent=tamanhoSenha;
+    numeroSenha.textContent = tamanhoSenha;
     geraSenha();
 }
 
-const camposenha = document.querySelector('#campo-senha');
+const campoSenha = document.querySelector('#campo-senha');
 
-const letrasMaiusculas= 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-const letrasMinusculas ='abcdefghijklmnopqrstuvwxyz';
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const letrasMinusculas = 'abcdefghijklmnopqrstuvwxyz';
 const numeros = '0123456789';
 const simbolos = '@*%4#!';
 geraSenha();
 
-function geraSenha(){
+function geraSenha() {
     let senha = '';
-for (let i =0; i<tamanhoSenha; i++){
-    let numeroAleatorio = Math.random()* letrasMaiusculas.length
-    numeroAleatorio= Math.floor (numeroAleatorio);
-    senha= + letrasMaiusculas[numeroAleatorio];
+    for (let i = 0; i < tamanhoSenha; i++) {
+        let numeroAleatorio = Math.random() * letrasMaiusculas.length
+        numeroAleatorio = Math.floor(numeroAleatorio);
+        senha = senha + letrasMaiusculas[numeroAleatorio];
 
-}
-camposenha.value= senha;
-
+    }
+    campoSenha.value = senha;
 }
 
 
